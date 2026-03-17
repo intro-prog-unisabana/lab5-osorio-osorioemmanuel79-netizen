45f123b4e68dad01b3456cb77bcfc88184871eb9
Sergio andres tiria lancheros <osorioemmanuel79@gmail.com>
2026-03-17 11:45:14 -0500

    functyv2

--- function_types.py
+++ function_types.py
@@ -1,9 +1,16 @@
-def promedio_estudiante(calificaciones):
-    if len(calificaciones) == 0:
-        return 0.0
-    
-    promedio = sum(calificaciones) / len(calificaciones)
-    return float(promedio)
+def list_shift(valores, incremento):
+    for i in range(len(valores)):
+        valores[i] = valores[i] + incremento
 
-notas = [85, 92, 78]
-print(promedio_estudiante(notas))
+def calc_avg(valores):
+    if len(valores) == 0:
+        return 0.0
+    return float(sum(valores) / len(valores))
+
+def print_normalized(valores):
+    print(valores)
+
+datos = [2.0, 4.0, 6.0, 8.0]
+prom = calc_avg(datos)
+list_shift(datos, -prom)
+print_normalized(datos)
